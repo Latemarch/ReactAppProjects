a4e02abbf2bcb45dc4a72d6d57921f41c674538b
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-03-10 00:30:49 +0900

    made error2

--- src/Shoppy/hooks/useProducts.js
+++ src/Shoppy/hooks/useProducts.js
@@ -2,7 +2,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { addNewProduct, getProducts } from '../apis/firebase'
 
 export default function useProducts() {
-  const queryClient = useQueryClient
+  // const queryClient = useQueryClient
   // const productsQuery = useQuery(['products'], getProducts)
   // const addProduct = useMutation(
   //   ({ product, url }) => addNewProduct(product, url),
