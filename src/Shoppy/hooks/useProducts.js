e9f0e6b2fbaa7f81b4335ec42707a439282e18bd
Latemarch <73418661+Latemarch@users.noreply.github.com>
2023-03-10 00:26:36 +0900

    made error

--- src/Shoppy/hooks/useProducts.js
+++ src/Shoppy/hooks/useProducts.js
@@ -3,11 +3,11 @@ import { addNewProduct, getProducts } from '../apis/firebase'
 
 export default function useProducts() {
   const queryClient = useQueryClient
-  const productsQuery = useQuery(['products'], getProducts)
-  const addProduct = useMutation(
-    ({ product, url }) => addNewProduct(product, url),
-    { onSuccess: () => queryClient.invalidateQueries(['products']) },
-  )
+  // const productsQuery = useQuery(['products'], getProducts)
+  // const addProduct = useMutation(
+  //   ({ product, url }) => addNewProduct(product, url),
+  //   { onSuccess: () => queryClient.invalidateQueries(['products']) },
+  // )
 
   return { productsQuery, addProduct }
 }
